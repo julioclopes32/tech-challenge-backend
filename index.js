879ff251e69cb3177e02a69c32532074b5906630
Julio Cesar Lopes <58558069+julioclopes32@users.noreply.github.com>
2021-11-22 10:34:45 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const app = express()
+const cors = require("cors")
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./tech-challenge-2ccfa-firebase-adminsdk-6scu7-5f55a9c7f4.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://tech-challenge-2ccfa-default-rtdb.firebaseio.com"
+});
+
+const db = admin.database();
+const ref = db.ref('server/saving-data/fireblog');
+
+const usersRef = ref.child('users');
+usersRef.set({
+  alanisawesome: {
+    date_of_birth: 'June 23, 1912',
+    full_name: 'Alan Turing'
+  },
+  gracehop: {
+    date_of_birth: 'December 9, 1906',
+    full_name: 'Grace Hopper'
+  }
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+    res.send("Hello World")
+});
+
+app.listen(3001, () => {
+    console.log("Server running in port 3001");
+});
