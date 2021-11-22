a561c259c090a093e5407d4b554304451c33097b
Julio Cesar Lopes <58558069+julioclopes32@users.noreply.github.com>
2021-11-22 18:49:28 -0300

    Update app.js

--- app.js
+++ app.js
@@ -12,12 +12,16 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => { console.log(`Server listening on port ${PORT}`); });
 
-app.get('/', (req, res) => {
+app.use(function(req, res, next){
   res.setHeader("Access-Control-Allow-Origin", "*")
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Access-Control-Max-Age", "1800");
   res.setHeader("Access-Control-Allow-Headers", "content-type");
-  res.setHeader("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
+  res.setHeader("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" );
+  next();
+});
+
+app.get('/', (req, res) => {
   res.send({"foo": "bar"});
 });
 
