a4c37e997cd25a5674ead09b7b6ae430c03057a8
Julio Cesar Lopes <58558069+julioclopes32@users.noreply.github.com>
2021-11-23 08:43:51 -0300

    Update app.js

--- app.js
+++ app.js
@@ -29,16 +29,12 @@ app.get('/', (req, res) => {
 });
 
 app.post('/favorites', function (req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*")
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Max-Age", "1800");
-  res.setHeader("Access-Control-Allow-Headers", "content-type");
-  res.setHeader("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" );
   var post_body = req.body;
   console.log(post_body);
   res.send({"foo": "bar"});
+  /*
   const usersRef = ref.child('teste2');
-  usersRef.set({post_body});
+  usersRef.set({post_body});*/
 
 })
 
