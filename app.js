a71a7be9d4f5575ecb4bdc02324135dd4a7f6738
Julio Cesar Lopes <58558069+julioclopes32@users.noreply.github.com>
2021-11-23 16:11:36 -0300

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ admin.initializeApp({
 });
 
 const corsOptions ={
-  origin:'http://localhost:3000', 
+  origin:'https://tech-challenge-frontend.herokuapp.com', 
   credentials:true,            //access-control-allow-credentials:true
   optionSuccessStatus:200
 }
