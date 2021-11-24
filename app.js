9bc49c11d80d63c2cf7980a7b5faa06f0abb28d8
Julio Cesar Lopes <58558069+julioclopes32@users.noreply.github.com>
2021-11-24 11:32:18 -0300

    Update app.js

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => { console.log(`Server listening on port ${PORT}`); });
 
 app.all('*', function(req, res, next){
-  res.header("Access-Control-Allow-Origin", 'https://tech-challenge-frontend.herokuapp.com');
+  res.header("Access-Control-Allow-Origin", 'http://tech-challenge-frontend.herokuapp.com');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
